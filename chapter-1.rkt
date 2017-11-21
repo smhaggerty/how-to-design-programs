a9dac8db23e100544f42ee1e87144ebc353f2920
Shane Haggerty <shanehaggerty@umail.ucsb.edu>
2017-11-20 20:45:14 -0800

    added exercises 21 through 38

--- chapter-1.rkt
+++ chapter-1.rkt
@@ -184,3 +184,176 @@
 ; str. Assume i is a number between 0 (inclusive) and the
 ; length of the given string (exclusive). See exercise 4
 ; for ideas. Can string-delete deal with empty strings?
+
+; Exercise 21:
+; Use DrRacket's stepper to evaluate (ff (ff 1))
+; step-by-step. Also try (+ (ff 1) (ff 1)). Does
+; DrRacket's stepper reuse the results of computations?
+
+
+
+; Exercise 22:
+; Use DrRacket's stepper on this programs fragment:
+;  (define (distance-to-origin x y)
+;    (sqrt (+ (sqr x) (sqr y))))
+; (distance-to-origin 3 4)
+
+; Exercise 23:
+; The first 1String in "hello world" is "h". How does the
+; following function compute this result?
+;   (define (string-first s)
+;     (substring s 0 1))
+; Use the stepper to confirm your ideas
+
+
+
+; Exercise 24:
+; (define (==> x y)
+;   (or (not x) y)
+
+
+
+; Exercise 25:
+; Take a look at this attempt to solve exercise 17
+;   (define (image-classify img)
+;     (cond
+;       [(>= (image-height img) (image-width img)) "tall"]
+;       [(= (image-height img) (image-width img)) "square"]
+;       [(<= (image-height img) (image-width img) "wide")]))
+
+
+
+; Exercise 26:
+; What do you expect as the value of this program
+; (define (string-insert s i)
+;   (string-append (substring s 0 i)
+;                  "-"
+;                  (substring s i)))
+; (string-insert "helloworld" 6)
+; Confirm your expectation with DrRacket and its stepper
+
+
+
+; Exercise 27:
+; Our solution to the sample problem contains several
+; constants in the middle of functions. As "One Programs,
+; Many Definitions" already points out, it is best to give
+; names to such constants so that future readers
+; understant where these numbers come from. Collect all
+; definitions in DrRacket's definitions area and change
+; them so that all magic numbers are refactored into
+; constant definitions.
+
+
+
+; Exercise 28:
+; Determine the potential profit for these ticket prices:
+; $1, $2, $3, $4, and $5. Which price maximizes the profit
+; of the movie theater? Determine the best ticket price to
+; a dime.
+; 
+; Here is an alternate version of the same program, given
+; as a single function definition
+; (define (profit price)
+;   (- (* (+ 120
+;            (* (/ 15 0.1)
+;               (- 5.0 price)))
+;         price)
+;      (+ 180
+;         (* 0.04
+;            (+ 120
+;               (* (/ 15 0.1)
+;                  (- 5.0 price)))))))
+; Enter this definition into DrRacket and ensure that it
+; produces the same results as the original version for
+; $1, $2, $3, $4, and $5.
+
+
+
+; Exercise 29:
+; After studying the costs of a show, the owner discovered
+; several ways of lowering the cost. As a result of these
+; improvements, there is no longer a fixed cost; a variable
+; cost of $1.50 per attendee remains. Modify both programs
+; to reflect this change. When the programs are modified,
+; test them again with ticket prices of $3, $4, and $5 and
+; compare the results.
+
+
+
+; Exercise 30:
+; Define constants for the price optimization program at
+; the movie theater so that the price sensitivity of
+; attendance (15 people for every 10 cents) becomes a
+; computed constant.
+
+
+
+; Exercise 31:
+; Recall the letter program from Composing Functions. Here
+; is how to launch the program and have it write its output
+; to the interactions area:
+; > (write-file
+;     'stdout
+;     (letter "Matthew" "Fisler" "Felleisen"))
+; Dear Matthew,
+; 
+; We have discovered that all people with the
+; last name Fisler have won our lottery. So, 
+; Matthew, hurry and pick up your prize.
+;
+; Sincerely, 
+; Felleisen
+; 'stdout
+;
+; Of course, programs are useful because you can launch
+; them for many different inputs. Run letter on three
+; inputs of your choice.
+
+
+
+; Exercise 32:
+; Most people no longer use desktop computers just to
+; run applications but also employ cell phones, tablets,
+; and their cars’ information control screen. Soon people
+; will use wearable computers in the form of intelligent
+; glasses, clothes, and sports gear. In the somewhat more
+; distant future, people may come with built-in bio
+; computers that directly interact with body functions.
+; Think of ten different forms of events that software
+; applications on such computers will have to deal with.
+
+
+
+; Exercise 33:
+; Research the “year 2000” problem and what it meant for
+; software developers.
+
+
+
+: Exercise 34:
+; Design the function string-first, which extracts the
+; first character from a non-empty string. Don’t worry
+; about empty strings.
+
+
+
+; Exercise 35: Design the function string-last, which
+; extracts the last character from a non-empty string.
+
+
+
+; Exercise 36: Design the function image-area, which
+; counts the number of pixels in a given image.
+
+
+
+; Exercise 37: Design the function string-rest, whic
+; produces a string like the given one with the first
+; character removed.
+
+
+
+; Exercise 38: Design the function string-remove-last,
+; which produces a string like the given one with the
+; last character removed.
